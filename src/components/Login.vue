f472d5e81a2960a31d8a17c46831a42c7151e7d2
mk <mkdodos@gmail.com>
2022-04-23 08:54:46 +0800

    login form

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,28 +1,55 @@
 <template>
-  <div @click="login">login</div>
+  <div>
+    <v-card elevation="2" 
+    class="mx-auto my-12"
+     max-width="374">
+      <v-card-title>Cafe Badilico</v-card-title>
+      <v-card-text>
+        <!-- <v-row align="center"> -->
+          <!-- <v-form> -->
+            <v-text-field label="帳號" v-model="email"></v-text-field>
+            <v-text-field label="密碼" v-model="password"></v-text-field>
+            <!-- <v-row class="mb-6"> -->
+            <!-- <v-col class="text-right"> -->
+              <v-btn color="#003554"  class="white--text" @click="login">登入</v-btn>
+            <!-- </v-col> -->
+            <!-- </v-row> -->
+          <!-- </v-form> -->
+        <!-- </v-row> -->
+      </v-card-text>
+    </v-card>
+  </div>
 </template>
 
 <script>
-import '../firebase/init';
+import "../firebase/init";
 
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
 const auth = getAuth();
-let email = "mk123@gmail.com";
-let password = "123456";
+// let email = "mk123@gmail.com";
+// let password = "123456";
 
 export default {
+  data() {
+    return {
+      email: "mk123@gmail.com",
+      password: "123456"
+    };
+  },
   methods: {
     login() {
       // firebase 登入
-      signInWithEmailAndPassword(auth, email, password).then(userCredential => {        
-        const user = userCredential.user; 
-        console.log(user.email)       
-        // 轉址
-        this.$router.replace("stock-transaction");        
-      });
+      signInWithEmailAndPassword(auth, this.email, this.password).then(
+        userCredential => {
+          const user = userCredential.user;
+          console.log(user.email);
+          // 轉址
+          this.$router.replace("stock-transaction");
+        }
+      );
     }
-  } 
+  }
 };
 </script>
 
