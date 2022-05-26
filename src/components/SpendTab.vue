876c615c5fc6ae730fe8668c077ca61f1fc2a1e1
mk <mkdodos@gmail.com>
2022-05-26 11:25:29 +0800

    cate name

--- src/components/SpendTab.vue
+++ src/components/SpendTab.vue
@@ -20,7 +20,9 @@
         <!-- 表格 -->
         <v-row>
           <v-col>
-            <v-data-table :headers="headers" :items="rows" :search="search.keyword"></v-data-table>
+            <v-data-table :headers="headers" :items="rows" :search="search.keyword">
+              <template v-slot:item.cateCht="{ item }">{{ getCateCht(item.cate) }}</template>
+            </v-data-table>
           </v-col>
         </v-row>
       </v-container>
@@ -49,18 +51,51 @@ export default {
       headers: [
         { text: "帳戶", value: "account", width: "100" },
         { text: "類別", value: "cate", width: "100" },
+        { text: "cate", value: "cateCht", width: "100" },
         { text: "日期", value: "spend_date", width: "100" },
         { text: "項目", value: "note", width: "200" },
-         { text: "收入", value: "income", width: "90" },
+        { text: "收入", value: "income", width: "90" },
         { text: "支出", value: "expense", width: "90" }
       ],
-      search: {}
+      search: { y: 2021 },
+      cates: [
+        { cate_id: 256, cate_name: "中文" },
+        { cate_id: 250, cate_name: "寶" },
+        { cate_id: 44, cate_name: "試試" }
+      ]
+      // cates: [{cate_id:256,cate_name:'中文'}]
     };
   },
   mounted() {
-    this.getData();
+    // this.getData();
+    this.getDataYM();
   },
   methods: {
+    // 取得類別名稱
+    getCateCht(cate) {
+      let name = "";
+      this.cates.forEach(row => {
+        if (row.cate_id == cate) name = row.cate_name;
+        console.log(row.cate_name);
+      });
+      return name;
+      // let id = this.cates.filter(row => (row.cate_id = cate));
+
+      // if (id.length > 0) return id[1].cate_name;
+      // else console.log(0);
+
+      // return "";
+
+      // let found = this.cates.find(element => element.cate_id = cate);
+      // console.log(found);
+      // if(found)
+      // return found.cate_name
+      // return ''
+
+      // var index = this.cates.indexOf();
+      // return cate+this.cates[0].cate_name
+    },
+
     async getDataYM() {
       this.rows = [];
       const citiesCol = collection(db, collection_name);
@@ -78,7 +113,7 @@ export default {
       docSnapBig.forEach(doc => {
         this.rows.push({ ...doc.data(), id: doc.id });
       });
-      console.log(this.rows);
+      // console.log(this.rows);
     },
     async getData() {
       // this.rows = [];
@@ -99,7 +134,7 @@ export default {
       docSnapBig.forEach(doc => {
         this.rows.push({ ...doc.data(), id: doc.id });
       });
-      console.log(this.rows);
+      // console.log(this.rows);
     }
   }
 };
