2146448ade6d190315d1e30f46926c55b4e1d940
mk <mkdodos@gmail.com>
2022-05-26 10:22:40 +0800

    ym

--- src/components/SpendTab.vue
+++ src/components/SpendTab.vue
@@ -1,20 +1,29 @@
 <template>
   <div>
     <v-app>
-    <v-container>
-      <v-row>
-        <v-col>
-          <v-text-field v-model="search" append-icon="mdi-magnify"></v-text-field>
-        </v-col>
-      </v-row>
+      <v-container>
+        <v-row>
+          <v-col>
+            <v-text-field v-model="search.keyword" append-icon="mdi-magnify"></v-text-field>
+          </v-col>
+          <v-col>
+            <v-text-field v-model="search.y" label="年"></v-text-field>
+          </v-col>
+          <v-col>
+            <v-text-field v-model="search.m" label="月"></v-text-field>
+          </v-col>
+          <v-col>
+            <v-btn @click="getDataYM">查詢</v-btn>
+          </v-col>
+        </v-row>
 
-      <!-- 表格 -->
-      <v-row>
-        <v-col>
-          <v-data-table :headers="headers" :items="rows" :search="search"></v-data-table>
-        </v-col>
-      </v-row>
-    </v-container>
+        <!-- 表格 -->
+        <v-row>
+          <v-col>
+            <v-data-table :headers="headers" :items="rows" :search="search.keyword"></v-data-table>
+          </v-col>
+        </v-row>
+      </v-container>
     </v-app>
   </div>
 </template>
@@ -23,13 +32,13 @@
 import db from "../db.js";
 import {
   collection,
-  getDoc,
-  doc,
+  // getDoc,
+  // doc,
   query,
   getDocs,
-  startAt,
-  // limit,
-  // where,
+  // startAt,
+  limit,
+  where,
   orderBy
 } from "firebase/firestore/lite";
 const collection_name = "spends";
@@ -38,32 +47,54 @@ export default {
     return {
       rows: [],
       headers: [
+        { text: "帳戶", value: "account", width: "100" },
+        { text: "類別", value: "cate", width: "100" },
         { text: "日期", value: "spend_date", width: "100" },
         { text: "項目", value: "note", width: "200" },
-        { text: "金額", value: "expense", width: "90" }
+         { text: "收入", value: "income", width: "90" },
+        { text: "支出", value: "expense", width: "90" }
       ],
-      search: ""
+      search: {}
     };
   },
   mounted() {
     this.getData();
   },
   methods: {
+    async getDataYM() {
+      this.rows = [];
+      const citiesCol = collection(db, collection_name);
+      const q = query(
+        citiesCol,
+        orderBy("spend_date", "desc"),
+        where(
+          "spend_date",
+          "<",
+          this.search.y + "-" + this.search.m + "-" + "01"
+        ),
+        limit(10)
+      );
+      const docSnapBig = await getDocs(q);
+      docSnapBig.forEach(doc => {
+        this.rows.push({ ...doc.data(), id: doc.id });
+      });
+      console.log(this.rows);
+    },
     async getData() {
       // this.rows = [];
       const citiesCol = collection(db, collection_name);
-      // const q = query(citiesCol, orderBy("spend_date", "desc"), limit(100));
+      const q = query(citiesCol, orderBy("spend_date", "desc"), limit(100));
       // const q = query(citiesCol, orderBy("expense", "desc"),where('expense','>',1000), limit(100));
       // const q = query(citiesCol, orderBy("expense", "desc"),where('expense','>',100000), limit(100));
       // const q = query(citiesCol, orderBy("expense"), startAt(1000000));
-      
+
       // 找出某一筆資料 (假設該筆資料的 expense 為 3000)
-      const docSnap = await getDoc(doc(citiesCol, "0WtHUapzuUGo2ao9C2Qh"));
+      // const docSnap = await getDoc(doc(citiesCol, "0WtHUapzuUGo2ao9C2Qh"));
       // 再用此資料為條件 => startAt(3000)
       // const q = query(citiesCol, orderBy("expense"), startAt(docSnap));
-      const q = query(citiesCol, orderBy("expense"), startAt(4000));
+      // const q = query(citiesCol, orderBy("expense"), startAt(4000));
 
-      console.log(docSnap);
+      // console.log(docSnap);
       const docSnapBig = await getDocs(q);
       docSnapBig.forEach(doc => {
         this.rows.push({ ...doc.data(), id: doc.id });
